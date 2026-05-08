7c047ae8c039eb13e6f1f3c1209c70610669fde4
TomCatXue <3505164245@qq.com>
2026-05-08 17:25:14 +0800

    修改

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://xuetom.github.io/",
-	base: "/",
+	base: "/xuetom.github.io",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
